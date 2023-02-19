a096b84954dfd2ca77b882412adc44f953c845b3
DavidTG7 <davidthekoa@gmail.com>
2023-02-18 19:55:21 -0500

    Solution for challenge 20

---
+++ 20_totalSavoryItems.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    item: "🍭",
+    price: 2.99,
+    type: "sweet",
+    onSale: true,
+  },
+  {
+    item: "🍫",
+    price: 1.99,
+    type: "sweet",
+    onSale: true,
+  },
+  {
+    item: "🥫",
+    price: 1.99,
+    type: "savory",
+    onSale: false,
+  },
+  {
+    item: "🍬",
+    price: 0.89,
+    type: "sweet",
+    onSale: false,
+  },
+  {
+    item: "🥦",
+    price: 3.99,
+    type: "savory",
+    onSale: false,
+  },
+  {
+    item: "🍖",
+    price: 3.99,
+    type: "savory",
+    onSale: true,
+  },
+];
+
+/*
+Use reduce() and only reduce() to calculate and return 
+the total cost of only the savory
+items in the shopping cart.
+
+Expected output: 9.97  
+*/
+
+function totalSavory(arr) {
+  return arr
+    .reduce((total, { type, price }) => {
+      if (type === "savory") {
+        return total + price;
+      } else {
+        return total;
+      }
+    }, 0)
+    .toFixed(2);
+}
+
+console.log(totalSavory(data));
