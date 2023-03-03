d8f792936021e8f09e2536fe1a7d5b430f826fd7
DavidTG7 <davidthekoa@gmail.com>
2023-03-02 20:00:45 -0500

    Solution for challenge 29

---
+++ 29_emojiSlotMachine/index.js
@@ -0,0 +1,41 @@
+/*
+   Let's create an emoji slot machine! Replace the hardcoded 
+   data with random fruit emojis from an emojis API. 
+   
+   - Request emoji food data from the API resource below. Log it and look at it!
+        - Write a function that takes in the data and returns a new array of only
+        fruit emoji objects
+        - Write a function to get 9 random fruits from your new array of fruit 
+        
+   - Load nine random fruits into the slot machine
+*/
+
+const slotMachine = document.querySelector(".emoji-slots-game");
+slotMachine.innerHTML = "";
+const food =
+  "https://apis.scrimba.com/emojihub/api/all/category/food-and-drink";
+
+function makeFruitArray(arr) {
+  return arr.map((item) => `<li>${item}</li>`);
+}
+
+function getRandomFruits(arr) {
+  let fruits = [];
+
+  for (let i = 0; i < 9; i++) {
+    fruits.push(arr[Math.floor(Math.random() * arr.length)].htmlCode);
+  }
+  return fruits;
+}
+
+let randomFruits = [];
+
+// write your fetch request here
+
+fetch(food)
+  .then((response) => response.json())
+  .then((data) => (randomFruits = getRandomFruits(data)))
+  .then((res) => makeFruitArray(randomFruits))
+  .then((res) => {
+    res.forEach((item) => (slotMachine.innerHTML += item));
+  });
